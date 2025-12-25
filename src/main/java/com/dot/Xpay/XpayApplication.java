079a1703fb5f4ee85dae523ddb483ce3e909ab0f
blossom.efosa <blossom.efosa@globalaccelerex.com>
2025-12-25 08:01:15 +0100

    Initial commit

---
+++ src/main/java/com/dot/Xpay/XpayApplication.java
@@ -0,0 +1,13 @@
+package com.dot.Xpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpayApplication.class, args);
+	}
+
+}
